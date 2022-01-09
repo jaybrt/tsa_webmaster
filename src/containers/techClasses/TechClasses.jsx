a627e4f15f66630c92ebf67974fd6480c56e8f5a
Jay Landers <jay@jaybirdgroup.net>
2022-01-09 01:36:12 -0700

    navbar and overhaul file structure

---
+++ src/containers/techClasses/TechClasses.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./techclasses.css";
+
+const TechClasses = () => {
+  return (
+    <div>
+      <h1>Tech Classes</h1>
+    </div>
+  );
+};
+
+export default TechClasses;
