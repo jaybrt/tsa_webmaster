e739f0eec0d006f5fe8ce57c66d2246f994ea2c7
Jay Landers <jay@jaybirdgroup.net>
2022-01-09 21:44:53 -0700

    created and styled header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import "./header.css";
+import lhsImg from "../../assets/lhsImg.png";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="lhs__header section__padding" id="home">
+      <div className="lhs__header-content">
+        <h1 className="gradient__text">
+          Learn about Littleton High School's Industry Leading STEM Program
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempor
+          orci eu lobortis elementum nibh tellus molestie. Varius morbi enim
+          nunc faucibus a pellentesque.
+        </p>
+
+        <div className="lhs__header-content__input">
+          <input type="email" placeholder="Your Email Address"></input>
+          <button type="button">Get In Contact</button>
+        </div>
+      </div>
+      <div className="lhs__header-image">
+        <img src={lhsImg} alt="LHS-Img"></img>
+      </div>
     </div>
   );
 };
